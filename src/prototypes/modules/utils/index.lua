6ad421b5e48862d50301fae8fd8eaa4b0ac13c5a
nyafnir <leonid.b4@yandex.ru>
2022-04-30 19:48:40 +0700

    feat: utils - add getConfig, giveItemToPlayer

--- src/prototypes/modules/utils/index.lua
+++ src/prototypes/modules/utils/index.lua
@@ -1,2 +1,12 @@
+-- Файл вспомогательных функций
 
--- giveItem(playerId, itemName)
+--- [Метод] Получить значение настройки без указания префикса
+function getConfig(name)
+    return settings.startup['multiplayer-teams:' .. name].value
+end
+
+--- [Метод] Выдать предмет игроку
+function giveItemToPlayer(playerId, itemName, amount)
+    local player = game.players[playerId]
+    player.insert({name=itemName, count=amount})
+end
