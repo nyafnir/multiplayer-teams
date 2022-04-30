8e830f447ec32b9b7a7c2abe2fabd50765478fce
Иван Караков <karakov7@gmail.com>
2022-04-30 20:40:05 +0300

    успешное вычисление для цены лаборатории

--- src/prototypes/modules/economy/index.lua
+++ src/prototypes/modules/economy/index.lua
@@ -1,13 +1,5 @@
 ---при первом запуске проинициализировать цену продукции
 
-local DELAY = 20
-
-local function delay()
-    global.tickswait = 0
-    while global.tickswait >= DELAY do
-    end   
-end
-
 function initPrices (player)
     --for _, p in pairs(game.item_prototypes) do
     --    player.print(p.name)
@@ -17,40 +9,47 @@ function initPrices (player)
     --end
 
     local function checkReceipt(name)
-        player.print('in')
-        player.print(name)
         -- if not player.force.recipes[name] then
         --save Table [name; price] --жидкость 0,01 прайс -- вода 0
         --    player.print('with 1 out for: '..name)
         --    return 1
         --end
-        if not player.force.recipes[name] then
-            return
+        if type(name) == 'string' then
+            local receipt = player.force.recipes[name]
+            
+            if receipt == nil then
+                return 1
+            end
+            
+            local price = 0
+
+            for _, ingredient in pairs(receipt.ingredients) do
+                price = price + checkReceipt(game.item_prototypes[ingredient.name].name) * ingredient.amount
+            end      
+
+            local countProducts = 0
+            
+            for _, product in pairs(receipt.products) do
+                countProducts = countProducts + product.amount
+            end
+            
+            local result = math.ceil(price / countProducts + 1)
+            return result
+
+        else
+            player.print("checkReceipt: "..name.name)
         end
-        for _, p in pairs(player.force.recipes[name].ingredients) do
-            player.print(p.name..' '..p.amount)
-          
-            checkReceipt(game.item_prototypes[p.name])
-        end
-        player.print('out for: '..name)
     end
+   
+    local price = 0
+   
     for _, p in pairs(player.force.recipes['lab'].ingredients) do
-        player.print('start')
-        player.print(p.name..' '..p.amount)
-        delay()
-        checkReceipt(p.name)
-        player.print('end')
+        price = price + checkReceipt(p.name) * p.amount
         --save Table [name; price]
     end
-
-
-end
-
-function onTick(event)
-    global.tickswait += 1
-    if global.tickswait == 1000 then
-        global.tickswait = 0
-    end
+    
+    price = price + 1
+    player.print('end. price = '..price)
 end
 
 
