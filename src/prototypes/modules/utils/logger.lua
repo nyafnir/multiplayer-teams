0da72ef00aac094ade9193c06fcb3a0816d799cb
nyafnir <leonid.b4@yandex.ru>
2022-05-03 19:37:08 +0700

    Логгер выкидывал ошибку на nil

--- src/prototypes/modules/utils/logger.lua
+++ src/prototypes/modules/utils/logger.lua
@@ -1,6 +1,10 @@
 --- [Метод] Вывести сообщение в чат (вывод отключается через настройки)
 function logger(text)
     if getConfig('logger:enable') == true then
+        if text == nil then
+            text = 'nil'
+        end
+
         game.print(getConfig('logger:prefix') .. text) -- игровой чат
         print(getConfig('logger:prefix') .. text) -- TODO: куда записывается этот лог?
     end
