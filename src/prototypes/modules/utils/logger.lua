42d7cd3caa496c76f8a0c1841e1b5e99c17ac61f
nyafnir <leonid.b4@yandex.ru>
2022-05-03 08:21:59 +0700

    Лог в print

--- src/prototypes/modules/utils/logger.lua
+++ src/prototypes/modules/utils/logger.lua
@@ -1,6 +1,7 @@
 --- [Метод] Вывести сообщение в чат (вывод отключается через настройки)
 function logger(text)
     if getConfig('logger:enable') == true then
-        game.print(getConfig('logger:prefix') .. text)
+        game.print(getConfig('logger:prefix') .. text) -- игровой чат
+        print(getConfig('logger:prefix') .. text) -- TODO: куда записывается этот лог?
     end
 end
