656412de5bf29ef1636e41dd163cfcfa7224ff84
nyafnir <leonid.b4@yandex.ru>
2022-06-11 18:02:48 +0700

    Пробросил конфиги во внутрь модуля

--- src/prototypes/modules/spawns/config.lua
+++ src/prototypes/modules/spawns/config.lua
@@ -1,1 +1,17 @@
-return {}
+return {
+    enable = getConfig('spawns:enable'),
+    respawn = {
+        timeout = {
+            minutes = getConfig('spawns:respawn-timeout'),
+            ticks = convertMinutesToTicks(getConfig('spawns:respawn-timeout'))
+        }
+    },
+    archimedeanSpiral = {
+        distance = getConfig('spawns:archimedean-spiral:distance'),
+        step = getConfig('spawns:archimedean-spiral:step')
+    },
+    base = {
+        internalRadius = getConfig('spawns:base:internal-radius'),
+        externalRadius = getConfig('spawns:base:external-radius')
+    }
+}
