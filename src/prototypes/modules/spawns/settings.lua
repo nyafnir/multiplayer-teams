92cb9fc0a795dd95c55dde74aece4b66f4e2da6d
nyafnir <leonid.b4@yandex.ru>
2022-06-11 17:44:52 +0700

    Конфигурация модуля баз

--- src/prototypes/modules/spawns/settings.lua
+++ src/prototypes/modules/spawns/settings.lua
@@ -6,26 +6,45 @@ data:extend({{
     order = 'd-spawns-a'
 }, {
     type = 'int-setting',
-    name = 'multiplayer-teams:spawns:near-distance',
-    setting_type = 'startup',
-    default_value = 25,
-    minimum_value = 10,
-    maximum_value = 100,
-    order = 'd-spawns-n'
-}, {
-    type = 'int-setting',
-    name = 'multiplayer-teams:spawns:far-distance',
-    setting_type = 'startup',
-    default_value = 75,
-    minimum_value = 50,
-    maximum_value = 500,
-    order = 'd-spawns-f'
-}, {
-    type = 'int-setting',
     name = 'multiplayer-teams:spawns:respawn-timeout',
     setting_type = 'startup',
     default_value = 10, -- минуты
     minimum_value = 5,
     maximum_value = 60,
     order = 'd-spawns-r'
+}})
+
+-- Скрытые параметры:
+data:extend({{
+    type = 'int-setting',
+    name = 'multiplayer-teams:spawns:archimedean-spiral:distance',
+    setting_type = 'startup',
+    default_value = 50,
+    minimum_value = 0,
+    order = 'd-spawns-z',
+    hidden = true
+}, {
+    type = 'int-setting',
+    name = 'multiplayer-teams:spawns:archimedean-spiral:step',
+    setting_type = 'startup',
+    default_value = 2,
+    minimum_value = 0,
+    order = 'd-spawns-z',
+    hidden = true
+}, {
+    type = 'int-setting',
+    name = 'multiplayer-teams:spawns:base:internal-radius',
+    setting_type = 'startup',
+    default_value = 96, --- это 3 чанка
+    minimum_value = 0,
+    order = 'd-spawns-z',
+    hidden = true
+}, {
+    type = 'int-setting',
+    name = 'multiplayer-teams:spawns:base:external-radius',
+    setting_type = 'startup',
+    default_value = 600, --- это 23 чанка
+    minimum_value = 0,
+    order = 'd-spawns-z',
+    hidden = true
 }})
