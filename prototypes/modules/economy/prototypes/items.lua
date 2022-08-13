7f1c992dc6671b5c0959d2f1c7be7d5f2a224c60
nyafnir <leonid.b4@yandex.ru>
2022-08-13 21:58:01 +0700

    Правка пути graphics и линтинг

--- prototypes/modules/economy/prototypes/items.lua
+++ prototypes/modules/economy/prototypes/items.lua
@@ -3,7 +3,7 @@ local ROOT = '__MultiplayerTeams__'
 data:extend({{
     type = "item",
     name = "shop-buy",
-    icon = "__MultiplayerTeams__/graphics/icons/shop-buy/shop.png",
+    icon = "__MultiplayerTeams__/prototypes/graphics/icons/shop-buy/shop.png",
     icon_size = 64,
     icon_mipmaps = 4,
     subgroup = "storage",
