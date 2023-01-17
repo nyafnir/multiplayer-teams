164dbd655e978b4b334211ff3246656feb100d51
nyafnir <nyafirawr@gmail.com>
2023-01-18 03:11:36 +0700

    Переопределение списка настроек проекта

--- settings.lua
+++ settings.lua
@@ -1,11 +1,12 @@
-require('prototypes.modules.utils.settings') -- order: 99
-
-require('prototypes.modules.teams.settings') -- order: 2
-require('prototypes.modules.spawns.settings') -- order: 3
-require('prototypes.modules.relations.settings') -- order: 4
--- require('prototypes.modules.economy.settings') -- order: 5
-
-require('prototypes.gui.settings') -- order: 1
-
 --- Настройки отображаемые в `Настройки модов`
 --- https://wiki.factorio.com/Tutorial:Mod_settings#Creation
+---
+require('prototypes.services.logger.settings') -- order: 99
+---
+require('prototypes.modules.gui.settings') -- order: 1
+---
+require('prototypes.modules.teams.settings') -- order: 2
+-- require('prototypes.modules.spawns.settings') -- order: 3
+-- require('prototypes.modules.relations.settings') -- order: 4
+-- require('prototypes.modules.economy.settings') -- order: 5
+require('prototypes.modules.quests.settings') -- order: 6
