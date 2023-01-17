a1f49768200e9d4b6d0de5872cdbe453071defaf
nyafnir <nyafirawr@gmail.com>
2023-01-18 03:12:32 +0700

    Обновление файла инициализации мода

--- control.lua
+++ control.lua
@@ -1,29 +1,38 @@
-require('prototypes.modules.utils.index')
+---@diagnostic disable: lowercase-global
 
-teams = require('prototypes.modules.teams.index')
-spawns = require('prototypes.modules.spawns.index')
-relations = require('prototypes.modules.relations.index') -- После `teams`
--- economy = require('prototypes.modules.economy.index') -- После `teams`
+---Импорты используемые по проекту
+---Порядок зависит от использования одного модуля другим
 
---- Последний, так как собирается из остальных модулей
-local gui = require('prototypes.gui.index')
+require('prototypes.utils.index')
+require('prototypes.services.index')
+
+offerModule = require('prototypes.modules.offers.module')
+-- spawnModule = require('prototypes.modules.spawns.module')
+teamModule = require('prototypes.modules.teams.module') -- Использует `spawnModule`, `offerModule`
+-- relationModule = require('prototypes.modules.relations.module') -- Использует `teamModule`, `offerModule`
+-- economyModule = require('prototypes.modules.economy.module') -- Использует `teamModule`
+questModule = require('prototypes.modules.quests.module') -- Использует `economyModule`
+guiModule = require('prototypes.modules.gui.module') -- Последний, так как собирается из остальных модулей
 
 --- [Событие] Происходит после создания карты с включенным модом
 script.on_init(function()
-    teams.on_init()
-    spawns.on_init()
-    relations.on_init()
-
-    gui.on_init()
+    offerModule.on_init()
+    teamModule.on_init()
+    -- economyModule.on_init()
+    -- relationModule.on_init()
+    -- spawnModule.on_init()
+    questModule.on_init()
+    guiModule.on_init()
 end)
 
 --- [Событие] Происходит после загрузки и сохранения
+--- "on_load() никогда не должен изменять `global`!"
 script.on_load(function()
-    --- И помните! "on_load() никогда не должен изменять `global`!"
-
-    teams.on_load()
-    spawns.on_load()
-    relations.on_load()
-
-    gui.on_load()
+    offerModule.on_load()
+    teamModule.on_load()
+    -- economyModule.on_load()
+    -- relationModule.on_load()
+    -- spawnModule.on_load()
+    questModule.on_load()
+    guiModule.on_load()
 end)
