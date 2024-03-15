b4c1e5bcff022bba87bf1eb97647f87bdef4a642
nyafnir <nyafirawr@gmail.com>
2024-03-15 23:00:31 +0700

    fix(teams): delete redeclare var

--- src/prototypes/modules/teams/events/resolve-invite.lua
+++ src/prototypes/modules/teams/events/resolve-invite.lua
@@ -17,7 +17,7 @@ script.on_event(TEAM_INVITE_RESOLVE_EVENT_NAME,
         if event.resolve then
             LoggerService.chat({ 'mt.teams.invite.result.yes', player.name, team.title }, nil, owner.force)
 
-            local status, result = pcall(TeamModuleService.changeTeamOfPlayerByAdmin, team.title, player.name, nil)
+            status, result = pcall(TeamModuleService.changeTeamOfPlayerByAdmin, team.title, player.name, nil)
             if status == false then
                 LoggerService.chat(result, nil, player)
                 LoggerService.chat(result, nil, owner)
